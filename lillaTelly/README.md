a14047f9a679f78db9f372c185fb4160d63f8608
jcbellido <jcbellido@gmail.com>
2022-04-23 05:38:51 +0200

    Minimal example of confy

---
+++ lillaTelly/README.md
@@ -0,0 +1,9 @@
+# Running on Linux
+
+I tried to execute in a RaspberryPi4 with a stock image. I had to add:
+
+`apt-get install libxcb1-dev`
+
+`apt-get install libxcb-composite0-dev`
+
+Don't expect speed, tho.
--- lillaTelly/src/main.rs
+++ lillaTelly/src/main.rs
@@ -1,3 +1,34 @@
+use confy;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct LillaTellyConfiguration {
+    version: u8,
+    executed_number_of_times: u64,
+}
+
+impl Default for LillaTellyConfiguration {
+    fn default() -> Self {
+        Self {
+            version: Default::default(),
+            executed_number_of_times: Default::default(),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let mut conf: LillaTellyConfiguration = match confy::load("LillaTelly") {
+        Ok(o) => o,
+        Err(e) => panic!("{e}"),
+    };
+    conf.executed_number_of_times += 1;
+    println!(
+        "This is the time {} that you executed",
+        conf.executed_number_of_times
+    );
+    match confy::store("LillaTelly", &conf) {
+        Ok(_) => {}
+        Err(e) => panic!("{e}"),
+    }
 }
