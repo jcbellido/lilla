a3086350d32ce1f4c5b4eccb8e0e59130338b993
jcb <jcbellido@gmail.com>
2022-05-22 06:26:03 +0200

    Update source_target_configuration.rs

--- lillaTelly/src/source_target_configuration.rs
+++ lillaTelly/src/source_target_configuration.rs
@@ -3,6 +3,9 @@ use serde_derive::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct SourceTargetConfiguration {
+    /// path to a dir. I'm expecting the files to be side-by-side here
     pub source: String,
+    /// path to a `plex - friendly` dir. Inside there should be a structure of directories Season XX
+    /// and inside those dirs Files should be named SXXEYY-[original name]
     pub target: String,
 }
