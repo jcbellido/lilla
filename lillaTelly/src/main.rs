ddf4ff867a95ce9a2f9e2c66d2a64a8ba8913bbb
jcb <jcbellido@gmail.com>
2022-05-25 13:58:47 +0200

    clap hooked

--- lillaTelly/src/main.rs
+++ lillaTelly/src/main.rs
@@ -1,6 +1,9 @@
 use std::path::Path;
 
 use clap::Parser;
+use lillatelly::{
+    source_target_configuration::SourceTargetConfiguration, task_tv_show::TaskTvShow,
+};
 
 #[derive(Parser, Debug)]
 #[clap(version, long_about = None)]
@@ -16,21 +19,50 @@ struct Args {
 }
 
 fn main() {
-    let args = Args::parse();
+    println!("Remember to setup RUST_LOG=\"debug\"");
+    pretty_env_logger::init();
 
+    let args = Args::parse();
     let config_file = Path::new(&args.config);
 
     if !config_file.exists() || !config_file.is_file() {
-        panic!("Config file `{}` can't be found!", args.config);
+        log::error!("Config file `{}` can't be found!", args.config);
+        return;
     }
 
     if args.dry && args.force {
-        panic!("Can't have both `dry` and `force`");
+        log::error!("Can't have both `dry` and `force`");
+        return;
     }
 
+    log::info!("Read args: `{:#?}`", args);
 
-    let all_configurations =
-        serde_json::from_str::<Vec<SourceTargetConfiguration>>(source).unwrap();
+    let file = std::fs::File::open(config_file).expect("Had problems opening the file!");
+    let reader = std::io::BufReader::new(file);
 
-    println!("args: `{:#?}`", args);
+    let all_configurations: Vec<SourceTargetConfiguration> =
+        serde_json::from_reader(reader).expect("Had problems parsing the configuration file");
+
+    if args.dry {
+        for config in all_configurations {
+            match TaskTvShow::new(config) {
+                Ok(tts) => match tts.dry_run() {
+                    Ok(dr) => {
+                        log::info!(
+                            "Actions gatehered for {} - {}",
+                            tts.configuration.source,
+                            tts.configuration.target
+                        );
+                        for a in dr {
+                            log::info!("{:#?}", a);
+                        }
+                    }
+                    Err(e) => log::error!("Error, collecting dry run `{e}`"),
+                },
+                Err(e) => log::error!("Error while constructing task Tv Show: `{e}`"),
+            }
+        }
+    } else {
+        log::warn!("Non dry runs are still unimplemented, sorry!");
+    }
 }
